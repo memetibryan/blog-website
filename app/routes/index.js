316d399e5e18a004fb2084abde4e28d3f7aed12f
memetibryan <memetibryan@gmail.com>
2017-06-08 15:58:47 +0300

    included

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,16 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return Ember.RSVP.hash({
-      blogs: this.store.findAll('blog'),
-      reviews: this.store.findAll('review')
-    });
+    return this.store.findAll('blog');
   },
-  actions: {
-    saveBlog3(params) {
-      var newBlog = this.store.createRecord('blog', params);
-      newBlog.save();
-      this.transitionTo('index');
-    },
-  }
 });
